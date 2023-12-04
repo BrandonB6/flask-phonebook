1967997d7b2dfaf2ca7597cc982bfaac53e14808
BrandonB6 <babbehr6@gmail.com>
2023-12-03 18:28:56 -0600

    first commit

---
+++ forms.py
@@ -0,0 +1,12 @@
+# helps make sure users are giving the correct information
+
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email
+
+class UserLoginForm(FlaskForm):
+    # email, password, submit
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
